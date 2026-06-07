f682edf7a0c2f452a4d83fb742ce77d5543ff50f
samdev.sys <sv4432@gmail.com>
2026-06-07 09:35:55 -0500

    frontend_pagina

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/bigbang_university/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
